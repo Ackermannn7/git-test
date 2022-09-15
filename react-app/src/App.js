4317909a2f5be4e75d88d7a313e433ac178b0a43
Aleksey Rudyk <aleksey.rudyksm@gmail.com>
2022-09-15 13:19:22 +0300

    app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponents';
-function App() {
-  return (
+import './App.css';
+class App extends Component {
+  render(){
+    return(
     <div className="App">
       <Navbar dark color="primary">
         <div className='container'>
@@ -12,7 +13,7 @@ function App() {
       </Navbar>
       <Menu/>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
