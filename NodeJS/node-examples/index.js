911bc364c6192c959e6768f962c4f005cc07da96
Aleksey Rudyk <aleksey.rudyksm@gmail.com>
2022-10-24 10:29:16 +0300

    simple node module

--- NodeJS/node-examples/index.js
+++ NodeJS/node-examples/index.js
@@ -1,7 +1,4 @@
-var rect = {
-  perimeter: (x, y) => 2 * (x + y),
-  area: (x, y) => x * y,
-};
+var rect = require("./rectangle");
 
 function solveRect(l, b) {
   console.log("Solving for rectangle with l = " + l + " and b = " + b);
@@ -15,7 +12,7 @@ function solveRect(l, b) {
     );
   } else {
     console.log("The area of rectangle = " + rect.area(l, b)) + "\n";
-    console.log("The perimeter of the rectangle = " + rect.perimeter(l, b));
+    console.log("The perimeter of the rectangle = " + rect.perimeter(l, b)) + "\n";
   }
 }
 
