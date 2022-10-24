36e0c1e38cbbb55c9ca8ec60a7e11f4d643d07d7
Aleksey Rudyk <aleksey.rudyksm@gmail.com>
2022-10-24 10:24:29 +0300

    node.js commit

---
+++ NodeJS/node-examples/index.js
@@ -0,0 +1,25 @@
+var rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+function solveRect(l, b) {
+  console.log("Solving for rectangle with l = " + l + " and b = " + b);
+  if (l <= 0 || b <= 0) {
+    console.log(
+      "Rectangle dimensions should be greater than 0: l = " +
+        l +
+        ", and b = " +
+        b +
+        "\n"
+    );
+  } else {
+    console.log("The area of rectangle = " + rect.area(l, b)) + "\n";
+    console.log("The perimeter of the rectangle = " + rect.perimeter(l, b));
+  }
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
