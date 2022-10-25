59e3ddccdb984f5158db840926bd084d937201ab
Aleksey Rudyk <aleksey.rudyksm@gmail.com>
2022-10-25 12:53:13 +0300

    comm

--- NodeJS/node-examples/index.js
+++ NodeJS/node-examples/index.js
@@ -2,18 +2,29 @@ var rect = require("./rectangle");
 
 function solveRect(l, b) {
   console.log("Solving for rectangle with l = " + l + " and b = " + b);
-  if (l <= 0 || b <= 0) {
-    console.log(
-      "Rectangle dimensions should be greater than 0: l = " +
-        l +
-        ", and b = " +
-        b +
-        "\n"
-    );
-  } else {
-    console.log("The area of rectangle = " + rect.area(l, b)) + "\n";
-    console.log("The perimeter of the rectangle = " + rect.perimeter(l, b)) + "\n";
-  }
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log("Error: ", err.message);
+    } else {
+      console.log(
+        "The area of rectangle of dimensions l = " +
+          l +
+          ", and b = " +
+          b +
+          " = " +
+          rectangle.area()
+      );
+      console.log(
+        "The perimeter of rectangle of dimensions l = " +
+          l +
+          ", and b = " +
+          b +
+          " = " +
+          rectangle.perimeter()
+      );
+    }
+  });
+  console.log("This statement is after the call to rect()");
 }
 
 solveRect(2, 4);
